0a01a7a76effe292c7d8002641094a6f374e8622
Scott Moynes <scott.moynes@gmail.com>
2015-01-13 20:48:06 -0800

    Pick a theme.

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,17 +19,12 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+LINKS = None
+SOCIAL = None
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = "themes/pelican-sober"
