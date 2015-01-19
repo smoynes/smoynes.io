aa0a617ac5a3441313ab5496f8978fc2fc5f1573
Scott Moynes <scott.moynes@gmail.com>
2015-01-18 20:00:47 -0800

    Update blog configuration.

--- pelicanconf.py
+++ pelicanconf.py
@@ -25,6 +25,8 @@ SOCIAL = None
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 THEME = "themes/pelican-sober"
+
+DEFAULT_DATE = 'fs'
